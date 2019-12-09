2a24de77d3598e2f7d13ca14d96492dd8bbd7f57
marigliani <marigliani@UTILISA-RRKNBC2>
2019-12-09 14:46:28 +0100

    Fin de Mail

--- Git_Gedimat/Mail.cs
+++ Git_Gedimat/Mail.cs
@@ -21,6 +21,61 @@ namespace Git_Gedimat
         private string sujet = "Erreur de lecture du fichier csv";
         string body = "Les clients suivants n'ont pas été ajoutés à la base";
 
+        //constructeur d'un objet mail
+        public Mail(string unEnvoyeur, string nomEnvoyeur, string unReceveur)
+        {
+            this.from = unEnvoyeur;
+            this.fromname = nomEnvoyeur;
+            this.to = unReceveur;
+        }
 
+        public string GetFrom()
+        {
+            return this.from;
+        }
+
+        public string GetTo()
+        {
+            return this.to;
+        }
+        public string GetFromName()
+        {
+            return this.fromname;
+        }
+
+        public void EnvoieDuMail(string unReceveur, List<Client> ClientNonValide)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(this.from, this.fromname);
+            message.To.Add(new MailAddress(unReceveur));
+            message.Subject = this.sujet;
+            message.Body = this.body;
+            foreach(Client c in ClientNonValide)
+            {
+                message.Body += "\n" + c.GetRaisonSoc();
+            }
+
+            SmtpClient client = new SmtpClient(host);
+
+            //Pass SMTP credentials
+            client.Credentials = new System.Net.NetworkCredential(smtpUsername, smtpPasswrod);
+
+            //Enable SSL encryption
+            client.EnableSsl = true;
+            client.Port = this.port;
+
+            //Essaie d'envoie du mail. Montre le statu dans la console
+            try
+            {
+                Console.WriteLine("Tentative d'envoie du mail ...");
+                client.Send(message);
+                Console.WriteLine("Mail envoyé!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Le mail n'a pas été envoyé");
+                Console.WriteLine("Message d'erreur : " + ex.Message);
+            }
+        }
     }
 }
